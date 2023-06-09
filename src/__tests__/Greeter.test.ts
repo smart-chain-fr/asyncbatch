85f4b6926c5c85102ddb1687ae443f6762f0a978
Massi <massi.hamidouche@smart-chain.fr>
2023-06-10 01:05:51 +0200

    Setup Package

---
+++ src/__tests__/Greeter.test.ts
@@ -0,0 +1,4 @@
+import { Greeter } from "../main";
+test("My Greeter", () => {
+	expect(Greeter("Carl")).toBe("Hello Carl");
+});
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,1 @@
 export const Greeter = (name: string) => `Hello ${name}`;
-
-console.log(Greeter("hello"));
